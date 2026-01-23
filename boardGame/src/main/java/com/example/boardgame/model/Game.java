f5e2348857b15db294d4bcd5d158adf23614532c
Kacper Krasowski <krasovskyiekacper@gmail.com>
2026-01-23 23:31:56 +0100

    draft with game model

---
+++ boardGame/src/main/java/com/example/boardgame/model/Game.java
@@ -0,0 +1,66 @@
+package com.example.boardgame.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private double rentPrice;
+
+    private String title;
+    private String category;
+
+
+    private boolean isAvailable;
+
+    public Game() {}
+
+    public Game(String title, String category, double rentPrice){
+        this.title = title;
+        this.category = category;
+        this.rentPrice = rentPrice;
+        isAvailable = true;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getRentPrice() {
+        return rentPrice;
+    }
+
+    public void setRentPrice(double rentPrice) {
+        this.rentPrice = rentPrice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
